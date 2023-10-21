7a5932e2f6aeec000d5d2caee3e5c4dd30c22bd3
Tim Weiß <tim@weiss.moe>
2023-10-21 12:56:39 +0200

    use correct version

--- src/main/java/io/codetrail/codetrailintellij/rpc/RPCResponse.java
+++ src/main/java/io/codetrail/codetrailintellij/rpc/RPCResponse.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "action")
 @JsonSubTypes({
     @JsonSubTypes.Type(value = ConnectIDEResponse.class, name = "connectIDE"),
+    @JsonSubTypes.Type(value = AnnotateResponse.class, name = "annotate")
 })
 public abstract class RPCResponse {
 
